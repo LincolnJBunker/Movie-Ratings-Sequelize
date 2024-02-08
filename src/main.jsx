32dc85cfc275f50f4bd072136682f0b5ee3b7104
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-02-08 16:01:26 -0700

    lab 3 is done

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import ErrorPage from './pages/ErrorPage.jsx';
 import IndexPage from './pages/IndexPage.jsx';
 import AllMoviesPage from './pages/AllMoviesPage.jsx';
 import axios from 'axios';
+import MovieDetailPage from './pages/MovieDetailPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,15 @@ const router = createBrowserRouter(
           return { movies: res.data };
         }}
         />
+
+        <Route
+          path='movies/:movieId'
+          element={<MovieDetailPage />}
+          loader={async ({ params }) => {
+            const res = await axios.get(`/api/movies/${params.movieId}`);
+            return { movie: res.data };
+          }}
+          />
     </Route>,
   ),
 );
--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,13 +1,19 @@
+import { over } from 'lodash';
 import { useLoaderData } from 'react-router-dom';
 
 export default function MovieDetailPage() {
-  const response = useLoaderData();
-  const movieDetail = response.data;
+  const {
+    movie: { title, posterPath, overview },
+  } = useLoaderData();
+
+  // const response = useLoaderData();
+  // const movieDetail = response.data;
 
   return (
     <>
-      <h1>Movie Detail Page</h1>
-      <p>TODO</p>
+      <h1>{ title }</h1>
+      <img src={posterPath} alt={title} style={{ width: '200px' }} />
+      <p>{ overview }</p>
     </>
   );
 }
